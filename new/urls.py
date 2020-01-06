ad4b264d723626e93eda1a61a9bef8f426a107c0
just534 <273510287@qq.com>
2020-01-06 14:52:05 +0800

    '1'

--- new/urls.py
+++ new/urls.py
@@ -5,4 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.index1,name='index1'),
     path('index2/', views.index2,name='index2'),
+    path('index3/', views.index3,name='index3'),
+    path('index4/', views.index4,name='index4'),
+    path('index5/', views.index5,name='index5'),
+    path('index6/', views.index6,name='index6'),
+    path('index7/', views.index7,name='index7'),
+    path('index8/', views.index8,name='index8'),
 ]
--- new/views.py
+++ new/views.py
@@ -10,7 +10,7 @@ def index1(request):
         print(row['id'])
     return  HttpResponse("查询成功")
 
-
+#
 def index2(request):
     rows=Student.objects\
         .annotate(count_num=Count('score__course'),total_score=Sum('score__number'))\
@@ -18,3 +18,44 @@ def index2(request):
     for row in rows:
         print(row)
     return  HttpResponse("查询成功2")
+
+#查询姓“李”的老师的个数；
+def index3(request):
+    teachercount=Teacher.objects.filter(name__startswith='李').count()
+    print(teachercount)
+    return  HttpResponse("查询成功3")
+
+def index4(request):
+    students=Student.objects.filter(score__course__id__in=[1,2]).values('id','name')
+    print(students)
+    return  HttpResponse("查询成功4")
+
+def index5(request):
+    students=Student.objects.filter(score__course__teacher__name='黄老师')
+    for student in students:
+        print(student.name)
+
+    return  HttpResponse("查询成功5")
+
+
+def index6(request):
+    students=Student.objects.annotate(num=Count('score__course',filter=Q(score__course__teacher__name='黄老师')))\
+        .filter(num=Course.objects.filter(teacher__name='黄老师').count()).values('id','name')
+    for student in students:
+        print(student)
+
+    return  HttpResponse("查询成功6")
+
+
+def index7(request):
+    students=Student.objects.filter(score__number__lt=60).values('id','name')
+    for student in students:
+        print(student)
+
+    return  HttpResponse("查询成功7")
+def index8(request):
+    students=Student.objects.annotate(num=Count('score__course')).filter(num__lt=Course.objects.count())
+    for student in students:
+        print(student.name)
+
+    return  HttpResponse("查询成功8")
